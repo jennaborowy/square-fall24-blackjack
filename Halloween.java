d01c2b7f4507f36968a26744e79048c9f1192051
baseb <cmplowman@loyola.edu>
2024-09-16 15:46:19 -0400

    chris's halloween costume

--- Halloween.java
+++ Halloween.java
@@ -2,6 +2,7 @@ public class Halloween {
   public static void main(String[] args)
   {
     System.out.println("My favorite halloween costume is the sheet ghost because it's a classic - Callie");
+    System.out.println("My favorite halloween costume is patrick star because it means they have good taste - Chris");
   }
 
 }
