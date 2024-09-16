d0c5c478dffb8f4e53e9aee63a54fbc911baabfd
Emma Heiser <esheiser@loyola.edu>
2024-09-16 15:52:39 -0400

    emma halloween

--- Halloween.java
+++ Halloween.java
@@ -3,6 +3,7 @@ public class Halloween {
   {
     System.out.println("My favorite halloween costume is the sheet ghost because it's a classic - Callie");
     System.out.println("My favorite halloween costume is patrick star because it means they have good taste - Chris");
+    System.out.println("My favorite halloween costume is a witch because it has cool props -Emma");
   }
 
 }
