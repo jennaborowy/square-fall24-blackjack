c567d77cdb7f900ece3d3637a27eecc6888daf68
Callie <ccwalker@loyola.edu>
2024-09-16 15:40:14 -0400

    adding Halloween.java to repo

---
+++ Halloween.java
@@ -0,0 +1,7 @@
+public class Halloween {
+  public static void main(String[] args)
+  {
+    System.out.println("My favorite halloween costume is the sheet ghost because it's a classic - Callie");
+  }
+
+}
