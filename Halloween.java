52c5759b95db9359dcefc333d9868ac9eb889102
jennaborowy <jenna.borowy@gmail.com>
2024-09-16 16:09:55 -0400

    Jenna's fav costume

--- Halloween.java
+++ Halloween.java
@@ -4,6 +4,7 @@ public class Halloween {
     System.out.println("My favorite halloween costume is the sheet ghost because it's a classic - Callie");
     System.out.println("My favorite halloween costume is patrick star because it means they have good taste - Chris");
     System.out.println("My favorite halloween costume is a witch because it has cool props -Emma");
+    System.out.println("My favorite halloween (group) costume is probably Napolean Dynamite and Pedro because it's silly - Jenna");
   }
 
 }
