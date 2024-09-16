141deff642df173f77a19b385a9d3c4f617d045f
jennaborowy <jenna.borowy@gmail.com>
2024-09-16 16:54:44 -0400

    created src folder

---
+++ src/team_asn_1/Halloween.java
@@ -0,0 +1,11 @@
+package team_asn_1;//package team_asn_1;
+
+public class Halloween {
+  public static void main(String[] args)
+  {
+    System.out.println("My favorite halloween costume is the sheet ghost because it's a classic - Callie");
+    System.out.println("My favorite halloween costume is patrick star because it means they have good taste - Chris");
+    System.out.println("My favorite halloween costume is a witch because it has cool props -Emma");
+    System.out.println("My favorite halloween (group) costume is probably Napoleon Dynamite and Pedro because it's silly - Jenna");
+  }
+}
