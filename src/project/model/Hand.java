3afee646591538fbca3051516ac096f33335dd46
Callie <ccwalker@loyola.edu>
2024-10-15 19:57:21 -0400

    adding Hand class to branch

---
+++ src/project/model/Hand.java
@@ -0,0 +1,46 @@
+package src.project.model;
+
+import java.util.ArrayList;
+
+public class Hand
+{
+  private ArrayList<Card> cards;
+  //use for dealer's hand
+  private boolean isDealer;
+  private int pointValue;
+
+  public Hand(ArrayList<Card> cards, boolean isDealer, int pointValue) {
+    this.cards = cards;
+    this.isDealer = isDealer;
+    this.pointValue = pointValue;
+  }
+
+  private ArrayList<Card> getHand() {
+    return cards;
+  }
+
+  public boolean isDealer() {
+    return isDealer;
+  }
+  public int getPointValue() {
+    return pointValue;
+  }
+  private void addCard(Card newCard) {
+    cards.add(newCard);
+    changePoints(newCard);
+  }
+
+  private int changePoints(Card newCard) {
+    pointValue += newCard.getValue();
+    return pointValue;
+  }
+
+  public String toString() {
+    String handString = "";
+    for (Card card : cards) {
+      handString += card.toString() + " ";
+    }
+    return handString;
+  }
+
+}
