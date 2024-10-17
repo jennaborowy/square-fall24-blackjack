950ebe46ea6c1c4e2e7820765c601acc5babf0fd
Callie <ccwalker@loyola.edu>
2024-10-17 15:08:13 -0400

    re-adding card, deck, hand in new file structure

---
+++ src/main/java/edu/loyola/model/Deck.java
@@ -0,0 +1,38 @@
+package src.main.java.edu.loyola.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck
+{
+  private ArrayList<Card> cardsUndealt;
+  String[] suitStr = {"H", "D", "C", "S"};
+  String[] rankStr = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+
+  public Deck() {
+    cardsUndealt = new ArrayList<>();
+    for (String suit: suitStr) {
+      for (String rank : rankStr) {
+        cardsUndealt.add(new Card(suit, rank));
+      }
+    }
+    shuffle();
+  }
+  public void shuffle() {
+    Collections.shuffle(cardsUndealt);
+  }
+
+  public Card dealCard() {
+    if(cardsUndealt.isEmpty()) {
+      return null;
+    }
+    return cardsUndealt.remove(cardsUndealt.size()-1);
+
+  }
+  public int remainingCard() {
+    return cardsUndealt.size();
+  }
+
+}
+
+
