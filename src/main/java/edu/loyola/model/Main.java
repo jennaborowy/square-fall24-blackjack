b90f464fc63810e22445570d21e26623860d34d5
Callie <ccwalker@loyola.edu>
2024-10-17 15:09:04 -0400

    adding Game, Main, and Player classes

---
+++ src/main/java/edu/loyola/model/Main.java
@@ -0,0 +1,13 @@
+package src.main.java.edu.loyola.model;
+
+import java.util.Scanner;
+
+public class Main
+{
+  public static void main (String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Player p1 = new Player("p1", 100);
+    Game game = new Game(p1);
+    game.start();
+  }
+}
