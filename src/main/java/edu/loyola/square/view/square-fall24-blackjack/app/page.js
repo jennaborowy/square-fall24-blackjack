a79e51d23f606f3827426a33c43b916cb637a2c7
emma-h1 <90633343+emma-h1@users.noreply.github.com>
2024-10-20 11:45:55 -0400

    navigate app page

--- src/main/java/edu/loyola/square/view/square-fall24-blackjack/app/page.js
+++ src/main/java/edu/loyola/square/view/square-fall24-blackjack/app/page.js
@@ -5,11 +5,24 @@ import React from "react";
 export default function Home() {
 
   return (
-      <div>
-        <main className={styles.main}>
-            <Link href="/signup">Signup</Link>
-            <Link href="/login">Login</Link>
-        </main>
-    </div>
+
+          <main className={styles.main}>
+              <img src={"/logo-transparent.png"}
+                   alt=""
+                   height={415}
+                   width={530}
+                   style={{alignSelf:"center"}}/>
+
+              <div className={styles.container}>
+                  <button className={styles.btn}>
+                      <Link href="/signup">Signup</Link>
+                  </button>
+                  <button className={styles.btn}>
+                      <Link href="/login">Login</Link>
+                  </button>
+              </div>
+
+          </main>
+
   );
 }
