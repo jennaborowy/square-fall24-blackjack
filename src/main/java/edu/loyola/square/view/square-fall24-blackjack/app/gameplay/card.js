8435af31805fd3113f31a38a328f597c9d72aa44
Callie <ccwalker@loyola.edu>
2024-10-21 22:43:40 -0400

    updated card.css, card.js and page to replicate dealer/player hand

--- src/main/java/edu/loyola/square/view/square-fall24-blackjack/app/gameplay/card.js
+++ src/main/java/edu/loyola/square/view/square-fall24-blackjack/app/gameplay/card.js
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import './card.css'
 
 const Card = ({ suit, rank }) => {
     const rankCode = rank === '10' ? '0' : rank;
     const imageUrl = `https://deckofcardsapi.com/static/img/${rankCode}${suit[0]}.png`;
     return (
-        <div className="card">
-          <div className="card-size">
-            <img src={imageUrl} alt={`${rank} of ${suit}`} />
+
+          <div className="cardArea">
+            <img src={imageUrl} alt={`${rank} of ${suit}`}/>
             <p>{rank} of {suit}</p>
           </div>
-        </div>
+
     );
 
 };
--- src/main/java/edu/loyola/square/view/square-fall24-blackjack/app/gameplay/page.js
+++ src/main/java/edu/loyola/square/view/square-fall24-blackjack/app/gameplay/page.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Card from './card';
-
+import './card.css'
 export default function CardDisplay() {
 
   return (
     <div>
-      <h1>Card Display</h1>
-      <Card suit="Hearts" rank="A" />
-      <Card suit="Spades" rank="10" />
-      <Card suit="Clubs" rank="Q" />
-      <Card suit="Diamonds" rank="K" />
+      <div className="cardDisplay">
+        <h1>Card Display</h1>
+        <div className="dealerHand-container">
+        <Card suit="Hearts" rank="A" />
+        <Card suit="Spades" rank="10" />
+        </div>
+        <div className= "playerHand-container">
+        <Card suit="Clubs" rank="Q" />
+        <Card suit="Diamonds" rank="K" />
+          <Card suit="Spades" rank="3" />
+        </div>
+      </div>
     </div>
     );
 }
