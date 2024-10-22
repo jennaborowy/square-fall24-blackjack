7ed45ab19079a1d6e0f778986cd3eea8288c5065
jennaborowy <jenna.borowy@gmail.com>
2024-10-22 19:21:40 -0400

    did create user post

--- src/main/java/edu/loyola/square/controller/UserController.java
+++ src/main/java/edu/loyola/square/controller/UserController.java
@@ -1,37 +1,82 @@
 package edu.loyola.square.controller;
 
+import edu.loyola.square.UserAlreadyExistsException;
 import edu.loyola.square.controller.repositories.UserRepository;
+import edu.loyola.square.model.dto.UserDTO;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import edu.loyola.square.model.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 @CrossOrigin
 public class UserController extends SpringBootServletInitializer {
 
-  public static UserRepository userRepository = null;
-  public static void setRepo(UserRepository repo) { userRepository = repo;}
+
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @GetMapping("/")
+  public List<User> all() {
+    return userRepository.findAll();
+  }
+
+  @PostMapping("/signup")
+  public User create(@RequestBody UserDTO userDTO) throws UserAlreadyExistsException {
+    String username = userDTO.getUsername();
+    String password = userDTO.getPassword();
+    String email = userDTO.getEmail();
+    String firstName = userDTO.getFirstName();
+    String lastName = userDTO.getLastName();
+
+    if (userRepository != null) {
+      List<User> list = userRepository.findByEmail(email);
+
+      if (!list.isEmpty()) {
+        throw new UserAlreadyExistsException("Account already exists for this email");
+
+      }
+    }
+
+    User user = new User();
+    user.setUsername(username);
+    user.setPassword(password);
+    user.setEmail(email);
+    user.setFirstName(firstName);
+    user.setLastName(lastName);
+
+    return userRepository.save(user);
+
+  }
 
   @PostMapping("/login")
-  public ResponseEntity<String> login(@RequestBody Map<String, String> login) {
-    String username = login.get("username");
-    String password = login.get("password");
+  public ResponseEntity<Object> login(@RequestBody UserDTO login) {
+
+    String username = login.getUsername();
+    String password = login.getPassword();
 
     if (userRepository == null) {
       return new ResponseEntity<>("user repository empty", HttpStatus.BAD_REQUEST);
     } else {
       List<User> list = userRepository.findByUsername(username);
+
       if (list.isEmpty()) {
         return new ResponseEntity<>("User doesn't exist", HttpStatus.UNAUTHORIZED);
       } else {
         User user = list.get(0);
+        if (!user.getPassword().equals(password)) {
+          return new ResponseEntity<>("Wrong password", HttpStatus.UNAUTHORIZED);
+        }
         return new ResponseEntity<>("Welcome back!", HttpStatus.OK);
       }
     }
